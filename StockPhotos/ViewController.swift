8f467d840bc75bd05810b152a597af31fd5b20ea
klconur <onurpro@gmail.com>
2019-08-02 22:36:12 +0300

    First commit

---
+++ StockPhotos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StockPhotos
+//
+//  Created by ONUR KILIC on 2.08.2019.
+//  Copyright © 2019 Onur Kilic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
